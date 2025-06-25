6dd0ec604aa155fa56042d80d5035d5dd8b180c3
Centauria <centauriavc@gmail.com>
2025-06-25 11:48:05 +0800

    parabolicInterpolation on matrix first dim

--- Sources/FastCQT/Pitch.swift
+++ Sources/FastCQT/Pitch.swift
@@ -1,5 +1,6 @@
 import Accelerate
 import Foundation
+import Plinth
 
 public func parabolicInterpolation(_ x: [Float]) -> [Float] {
     let n = x.count
@@ -22,6 +23,24 @@ public func parabolicInterpolation(_ x: [Float]) -> [Float] {
     return shifts
 }
 
+public func parabolicInterpolation(_ x: Matrix<Float>) -> Matrix<Float> {
+    let m = x.shape.columns
+    let n = x.shape.rows
+    precondition(n >= 3, "Input matrix rows must be at least 3")
+    let a = x[2..<n, 0..<m] + x[0..<n - 2, 0..<m] - 2.0 * x[1..<n - 1, 0..<m]
+    let b = (x[0..<n - 2, 0..<m] - x[2..<n, 0..<m]) / 2.0
+    let a_ = a.absolute()
+    let b_ = b.absolute()
+
+    var shifts: Matrix<Float> = .zeros(shape: x.shape)
+    shifts[1..<n - 1, 0..<m] =
+        (a_[0..<n - 2, 0..<m] - b_[0..<n - 2, 0..<m] >= 0)
+        * b[0..<n - 2, 0..<m]
+        / a[0..<n - 2, 0..<m]
+
+    return shifts
+}
+
 public func piptrack(
     y: [Float],
     sr: Float = 22050,
@@ -36,5 +55,6 @@ public func piptrack(
     let S = spectrogram(y: y, nFFT: nFFT, hopLength: hopLength, window: window, center: center)
     let fMin = max(fmin, 0)
     let fMax = min(fmax, sr / 2)
-
+    let fftFreqs = fftFrequencies(sr: sr, nFFT: nFFT)
+    let avg = gradient(y: S)
 }
