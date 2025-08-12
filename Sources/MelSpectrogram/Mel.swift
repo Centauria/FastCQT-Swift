4e0a286fbbebbdd265c8c782f6e2afa4e08b5acd
Centauria <centauriavc@gmail.com>
2025-08-12 11:13:40 +0800

    melscale freq banks

--- Sources/MelSpectrogram/Mel.swift
+++ Sources/MelSpectrogram/Mel.swift
@@ -16,7 +16,7 @@ func _hzToMel(freq: Float, slaney: Bool) -> Float {
         }
         return mels
     } else {
-        return 2595.0 * log10(1.0 + freq / 700)
+        return 2595.0 / log(10) * log1p(freq / 700)
     }
 }
 
@@ -45,14 +45,51 @@ func _melToHz(mels: [Float], slaney: Bool) -> [Float] {
     }
 }
 
-func melscaleBanks(
+func _createTriangularFilterbank(allFreqs: [Float], fPts: [Float]) -> Matrix<Float> {
+    let nFreqs = allFreqs.count
+    let nfDiff = fPts.count - 1
+    let nFilters = nfDiff - 1
+    let fDiff: [Float] = .init(unsafeUninitializedCapacity: nfDiff) { buffer, initializedCount in
+        let fd = buffer.baseAddress!
+        fPts.withUnsafeBufferPointer {
+            let fp = $0.baseAddress!
+            vDSP_vsub(fp, 1, fp.advanced(by: 1), 1, fd, 1, vDSP_Length(nfDiff))
+        }
+        initializedCount = nfDiff
+    }
+    let slopes: Matrix<Float> = .init(shape: .init(rows: nFreqs, columns: fPts.count)) { i, j in
+        fPts[j] - allFreqs[i]
+    }
+    let downSlopes: Matrix<Float> = .init(shape: .init(rows: nFreqs, columns: nFilters)) { i, j in
+        -slopes[i, j] / fDiff[j]
+    }
+    let upSlopes: Matrix<Float> = .init(shape: .init(rows: nFreqs, columns: nFilters)) { i, j in
+        slopes[i, j + 2] / fDiff[j + 1]
+    }
+    let fb = downSlopes.minimum(upSlopes).threshold(to: 0, with: .clampToThreshold)
+    return fb
+}
+
+public func melscaleFreqBanks(
     nFreqs: Int,
     fMin: Float,
     fMax: Float,
     nMels: Int,
     sampleRate: Float,
-    slaney: Bool
+    norm: Bool = false,
+    slaney: Bool = true
 ) -> Matrix<Float> {
     let allFreqs = vDSP.ramp(in: 0...floor(sampleRate / 2), count: nFreqs)
-    return .empty
+    let mMin = _hzToMel(freq: fMin, slaney: slaney)
+    let mMax = _hzToMel(freq: fMax, slaney: slaney)
+    let mPts = vDSP.ramp(in: mMin...mMax, count: nMels + 2)
+    let fPts = _melToHz(mels: mPts, slaney: slaney)
+    var fb = _createTriangularFilterbank(allFreqs: allFreqs, fPts: fPts)
+    if norm {
+        let enorm = vDSP.divide(2.0, vDSP.subtract(fPts[2..<nMels + 2], fPts[0..<nMels]))
+        for i in 0..<nMels {
+            fb[0..<nFreqs, i] *= enorm[i]
+        }
+    }
+    return fb
 }
